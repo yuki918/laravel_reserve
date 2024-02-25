ed2b1e73375e665438a0a8b575a324f5b498bb49
githunb yuki918 <tada950918@gmail.com>
2024-02-25 16:17:46 +0900

    イベントの予約

--- src/resources/views/dashboard.blade.php
+++ src/resources/views/dashboard.blade.php
@@ -1,14 +1,23 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            イベントカレンダー
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+              <div class="container p-10 mx-auto">
+                <div class="w-full mx-auto overflow-auto">
+                  @if (session('status'))
+                      <div class="mb-4 font-medium text-sm text-green-600">
+                          {{ session('status') }}
+                      </div>
+                  @endif
+                  @livewire('calendar')
+                </div>
+              </div>
             </div>
         </div>
     </div>
--- src/routes/web.php
+++ src/routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LivewireTestController;
 use App\Http\Controllers\AlpineTestController;
 use App\Http\Controllers\EventController;
+use App\Http\Controllers\ReservationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,25 +21,25 @@ Route::get('/', function () {
     return view('calendar');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+// Route::middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified'
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('dashboard');
+//     })->name('dashboard');
+// });
 
 Route::prefix('manager')->middleware('can:manager-higher')->group(function() {
   Route::get('events/past', [EventController::class, 'past'])->name('events.past');
   Route::resource('events', EventController::class);
 });
+
 Route::middleware('can:user-higher')->group(function() {
-  Route::get('index', function () {
-      // return view('welcome');
-      dd('user');
-  });
+  Route::get('dashboard', [ReservationController::class, 'dashboard'])->name('dashboard');
+  Route::get('{id}', [ReservationController::class, 'detail'])->name('events.detail');
+  Route::post('{id}', [ReservationController::class, 'reserve'])->name('events.reserve');
 });
 
 Route::controller(LivewireTestController::class)
